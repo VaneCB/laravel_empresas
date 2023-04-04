f2968dbc92553c7b23031641f017d5c9863335b3
vanesa <vcarrerabcn@gmail.com>
2023-04-04 14:19:36 +0200

    metodo para ordenar la tabla por campos

--- resources/js/componentes/mitabla.vue
+++ resources/js/componentes/mitabla.vue
@@ -2,7 +2,7 @@
     <table class="bg-[#dfe7f8d9] table auto border-collapse border-spacing-2 border border-slate-400 mx-auto">
         <caption>{{ tabla }}</caption>
         <tr>
-            <th class="border border-slate-300 bg-[#0c4a6e] text-white" v-for='(campo) in campos' >{{ campo }}</th>
+            <th class="border border-slate-300 bg-[#0c4a6e] text-white" v-for='(campo) in campos' ><button @click='ordenar(campo)'>{{ campo }}</button></th>
         </tr>
         <tr v-for='(fila) in filas'>
             <td class="border border-slate-300 p-2" v-for='(valor) in fila'>{{ valor }}</td>
@@ -24,6 +24,16 @@ export default {
     mounted() {
         this.filas= JSON.parse(this.filas_serializadas)
         this.campos= JSON.parse(this.campos_serializados)
+    },
+    methods: {
+        ordenar: function (campo) {
+            this.filas = this.filas.sort((a,b)=>{
+                if (a[campo]>b[campo])
+                    return 1;
+                else
+                    return -1;
+            });
+        }
     }
 }
 </script>
